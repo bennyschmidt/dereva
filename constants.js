81709108994520a4723c5290f79969cb2afcc036
exactchange <benny@exactchange.network>
2022-09-28 11:18:15 -0700

    remove unused deps

--- constants.js
+++ constants.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const {
-  API_KEY,
   API_URL,
   DATA_URI,
   HOST,
@@ -15,7 +14,6 @@ const {
 } = process.env;
 
 module.exports = {
-  API_KEY,
   API_URL,
   DATA_URI,
   HOST,

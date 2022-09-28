f4a5b8d008ba9fbc524fcef7a358333ccb53960a
exactchange <benny@exactchange.network>
2022-09-28 11:33:44 -0700

    remove old peers

--- peers.js
+++ peers.js
@@ -1,6 +1,3 @@
 module.exports = {
-  "ff0b0fe7-1d46-764a-2b82-9751147cbdd8": "https://www.exactchange.network/dereva",
-  "da6f75c5-4e72-ae4a-82ff-9dc84ecd5fb1": "https://www.exactchange.network/mrtr",
-  "3840e0ad-256b-4030-a52b-f011325ed31e": "https://www.exactchange.network/reverse",
-  "3a36cb86-2f50-41bf-896b-1cbcf48d1cd2": "https://www.exactchange.network/cult"
+  "ff0b0fe7-1d46-764a-2b82-9751147cbdd8": "https://www.exactchange.network/dereva"
 };
